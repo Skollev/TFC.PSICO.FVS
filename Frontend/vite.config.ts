e6bde5f0ad2d4ee009534183a9446cb9d944e767
Skollev <153862848+Skollev@users.noreply.github.com>
2025-05-24 18:55:53 +0200

    fix: cambios en las variables

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const apiBase = process.env.VITE_API_BASE_URL || 'http://localhost:8080'
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
   server: {
     proxy: {
-      '/api': 'http://localhost:8080'
+      '/api': apiBase
     }
   }
 
