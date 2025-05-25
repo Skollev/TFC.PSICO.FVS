d27a46e84cf5a6705358ca25c9b2294feeb0d2a8
Skollev <153862848+Skollev@users.noreply.github.com>
2025-05-25 18:18:57 +0200

    fix; vite config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const apiBase = process.env.VITE_API_BASE_URL || 'http://localhost:8080'
-
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
