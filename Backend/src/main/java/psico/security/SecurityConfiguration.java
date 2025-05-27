9d3c05cc565e64fcd73ae4c776835bc685653f7c
Skollev <153862848+Skollev@users.noreply.github.com>
2025-05-27 17:56:39 +0200

    fix:Cambios delete

--- Backend/src/main/java/psico/security/SecurityConfiguration.java
+++ Backend/src/main/java/psico/security/SecurityConfiguration.java
@@ -47,13 +47,13 @@ public class SecurityConfiguration {
 				.requestMatchers(HttpMethod.POST, "/cita/pagar/{id}").hasAuthority("TERAPEUTA")
 				.requestMatchers(HttpMethod.POST, "/cita/confirmar/{id}").hasAuthority("PACIENTE")
 				.requestMatchers(HttpMethod.POST, "/cita/{id}").hasAuthority("PACIENTE")
-				.requestMatchers(HttpMethod.DELETE, "/cita/{id}").hasAuthority("PACIENTE")
+				.requestMatchers(HttpMethod.DELETE, "/cita/{id}").hasAnyAuthority("TERAPEUTA", "PACIENTE")
 
 				// INFORME SESION
 				.requestMatchers(HttpMethod.GET, "/informe/{id}").hasAnyAuthority("PACIENTE", "TERAPEUTA")
 				.requestMatchers(HttpMethod.GET, "/informe/cita/{id}").hasAnyAuthority("PACIENTE", "TERAPEUTA")
 				.requestMatchers(HttpMethod.POST, "/informe/{idCita}").hasAnyAuthority("TERAPEUTA", "PACIENTE")
-				.requestMatchers(HttpMethod.DELETE, "/informe/{id}").hasAuthority("TERAPEUTA")
+				.requestMatchers(HttpMethod.DELETE, "/informe/{id}").hasAnyAuthority("TERAPEUTA", "PACIENTE")
 
 				// PACIENTE
 				.requestMatchers(HttpMethod.GET, "/paciente").hasAuthority("TERAPEUTA")
