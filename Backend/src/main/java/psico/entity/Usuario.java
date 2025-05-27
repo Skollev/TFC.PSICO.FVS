f8e04b7a3b26fb06bb99e0340cb027d40d7d07b9
Skollev <153862848+Skollev@users.noreply.github.com>
2025-05-27 22:59:43 +0200

    fix: errores al desplegar

--- Backend/src/main/java/psico/entity/Usuario.java
+++ Backend/src/main/java/psico/entity/Usuario.java
@@ -18,11 +18,10 @@ public abstract class Usuario extends DomainEntity {
 	@NotBlank
 	private String apellido;
 
-	
 	@URL
+	@Column(length = 1000)
 	private String foto;
 
-	
 	@Email
 	private String correo;
 
@@ -38,23 +37,23 @@ public abstract class Usuario extends DomainEntity {
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	public String getApellido() {
 		return apellido;
 	}
-	
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	
+
 	public String getFoto() {
 		return foto;
 	}
-	
+
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
@@ -62,23 +61,23 @@ public abstract class Usuario extends DomainEntity {
 	public String getCorreo() {
 		return correo;
 	}
-	
+
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -86,8 +85,8 @@ public abstract class Usuario extends DomainEntity {
 	public void setRol(Roles rol) {
 		this.rol = rol;
 	}
-	
+
 	public Roles getRol() {
 		return rol;
 	}
-	}
+}
