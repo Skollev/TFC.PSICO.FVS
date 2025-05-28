06b7c2f3b73a78e80338a1e627b69f29023f5cb0
Skollev <153862848+Skollev@users.noreply.github.com>
2025-05-29 00:45:52 +0200

    style

--- Frontend/src/Components/PrecioCircular.tsx
+++ Frontend/src/Components/PrecioCircular.tsx
@@ -66,7 +66,6 @@ export default function PrecioCircular({ precio, texto }: Props) {
                 width="200"
                 height="200"
                 style={{
-                    color: "black",
                     position: "absolute",
                     top: 0,
                     left: 0,
@@ -84,7 +83,7 @@ export default function PrecioCircular({ precio, texto }: Props) {
                     />
                 </defs>
                 <text
-                    fill="#198754"
+                    fill="black"
                     fontSize="17"
                     letterSpacing="3px"
                 >
