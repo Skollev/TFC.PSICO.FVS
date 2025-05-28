5597d09e5a3b4596868c2a5b3ead6bcb30de1f23
Skollev <153862848+Skollev@users.noreply.github.com>
2025-05-28 20:41:58 +0200

    style:fecha península

--- Frontend/src/Components/FormularioModificarCita.tsx
+++ Frontend/src/Components/FormularioModificarCita.tsx
@@ -64,7 +64,7 @@ export default function FormularioModificarCita() {
                     }}
                 />
                 <CardContent sx={{ flexGrow: 1 }}>
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='de'>
                         <form onSubmit={handleSubmit}>
                             <Stack spacing={3}>
 
